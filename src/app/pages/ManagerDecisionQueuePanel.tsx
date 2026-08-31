a81ec820efad2137a276e732fe6804408e29f279
rokaddev <dev@rokad.co>
2026-08-31 06:33:33 +0530

    feat: add delivery SLO App Home surface

--- src/app/pages/ManagerDecisionQueuePanel.tsx
+++ src/app/pages/ManagerDecisionQueuePanel.tsx
@@ -3,6 +3,7 @@ import { Flex } from '@hubspot/ui-extensions';
 import { ExecutiveRevenuePanel } from './ExecutiveRevenuePanel';
 import { ManagerDecisionQueuePanel as ManagerDecisionQueueCore } from './ManagerDecisionQueueCore';
 import { RecommendationDeliveryAnalyticsPanel } from './RecommendationDeliveryAnalyticsPanel';
+import { RecommendationDeliverySloPanel } from './RecommendationDeliverySloPanel';
 import { RecommendationNotificationConfigurationPanel } from './RecommendationNotificationConfigurationPanel';
 import { RecommendationOperationsPanel } from './RecommendationOperationsPanel';
 import { RecommendationOutcomePanel } from './RecommendationOutcomePanel';
@@ -17,5 +18,6 @@ export function ManagerDecisionQueuePanel({ enabled }: { enabled: boolean }) {
     <RecommendationNotificationConfigurationPanel enabled={enabled} />
     <RecommendationRoutingPoliciesPanel enabled={enabled} />
     <RecommendationDeliveryAnalyticsPanel enabled={enabled} />
+    <RecommendationDeliverySloPanel enabled={enabled} />
   </Flex>;
 }
